a8d90a55d6480014fe20b105abe535587e9ea6a2
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-09-26 22:49:38 -0600

    feat(funciones):creacion de funciones auxiliares

--- main_interfaz.py
+++ main_interfaz.py
@@ -1,50 +1,20 @@
-from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer # Vectorizador      
+# ---------------------------| Bibliotecas |---------------------------
 import tkinter                                                              # Interfaz gráfica
-from tkinter import filedialog                                              # Cuadro de dialogo
 from paquetes.funciones import *                                            # Funciones para precargar información
 
-# --> Precargar información
+# ---------------------------| Precargar información |---------------------------
 
 # Cargar corpus
 corpus = leer_corpus('corpus/corpus_filtrado.txt')
 
 # Vector binario
-vectorizador_binario = CountVectorizer(binary=True, token_pattern=r'(?u)\w\w+|\w\w+\n|\.')
-X = vectorizador_binario.fit_transform(corpus)
-print('Representación vectorial binarizada')
-print(X.toarray())
-
-# Vector de frecuencia
-vectorizador_frecuencia = CountVectorizer(token_pattern=r'(?u)\w\w+|\w\w+\n|\.')
-X = vectorizador_frecuencia.fit_transform(corpus)
-print('Representación vectorial por frecuencia')
-print(X.toarray())
-
-# Vector TF-IDF
-vectorizador_tfidf = TfidfVectorizer(token_pattern=r'(?u)\w\w+|\w\w+\n|\.')
-X = vectorizador_tfidf.fit_transform(corpus)
-print('Representación vectorial tf-idf')
-print(X.toarray())
+vector_binario = crear_vector_binario(corpus)
+vector_frecuencia = crear_vector_frecuencia(corpus)
+vector_tfidf = crear_vector_tfidf(corpus)
 
 
 
-
-# --> Variables globales
-noticias_seleccionadas = None
-
-
-# --> Funciones
-
-def cargar_archivo():
-    """Función para cargar archivos txt"""
-    ruta_archivo = filedialog.askopenfilename(title="Abrir archivo", filetypes=(("Archivos de texto", "*.txt"),))
-    print(f'[+]La ruta es: {ruta_archivo}')
-    with open(ruta_archivo, "r", encoding="utf-8") as informacion:
-        noticias_seleccionadas = informacion.read().split('\n')
-    return noticias_seleccionadas
-
-
-# --> Inicio de interfaz
+# ---------------------------| Inicio de interfaz |---------------------------
 
 """Configuracion de ventana"""
 ventana = tkinter.Tk()
@@ -55,7 +25,7 @@ ventana.config(bg="white")
 """Contenido de ventana"""
 
 # Crear botones
-boton_cargar = tkinter.Button(ventana, text="Cargar archivo", fg="white", bg="green", font="bold", command=cargar_archivo)
+boton_cargar = tkinter.Button(ventana, text="Cargar archivo", fg="white", bg="green", font="bold")
 boton_salir = tkinter.Button(ventana, text="Salir", fg="white", bg="red", font="bold", command=ventana.quit)
 
 
--- paquetes/funciones.py
+++ paquetes/funciones.py
@@ -1,36 +1,93 @@
 # ---------------------| Bibliotecas |---------------------
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer  # Vectorizador
-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer    # Vectorizador
+from tkinter import filedialog                                                  # Cuadro de dialogo
+from spacy.lang.es.stop_words import STOP_WORDS                                 # Palabras vacias
+import spacy                                                                    # Procesamiento de lenguaje natural
 
 # ---------------------| Funciones |---------------------
 def leer_corpus(ruta):
-    """Función para leer el corpus"""
+    """Leer corpus preparado"""
 
     with open(ruta, "r", encoding="utf-8") as informacion:
         corpus = informacion.read().split('\n')
     return corpus
 
-def vector_binario(corpus):
+
+def cargar_archivo():
+    """Cargar archivo txt de usuario"""
+
+    ruta_archivo = filedialog.askopenfilename(title="Abrir archivo", filetypes=(("Archivos de texto", "*.txt"),))
+    print(f'[+]Ruta de archivo: {ruta_archivo}')
+    with open(ruta_archivo, "r", encoding="utf-8") as informacion:
+        noticias_seleccionadas = informacion.read().split('\n')
+    return noticias_seleccionadas
+
+
+def crear_vector_binario(corpus):
     """Función para vectorizar el corpus en binario"""
 
     vectorizador_binario = CountVectorizer(binary=True, token_pattern=r'(?u)\w\w+|\w\w+\n|\.')
     X = vectorizador_binario.fit_transform(corpus)
-    print('[+]Representación vectorial binarizada')
+    print('[+]Se creo el vector binario')
     return X.toarray()
 
-def vector_frecuencia(corpus):
+
+def crear_vector_frecuencia(corpus):
     """Función para vectorizar el corpus por frecuencia"""
 
     vectorizador_frecuencia = CountVectorizer(token_pattern=r'(?u)\w\w+|\w\w+\n|\.')
     X = vectorizador_frecuencia.fit_transform(corpus)
-    print('Representación vectorial por frecuencia')
+    print('[+]Se creo el vector de frecuencia')
     return X.toarray()
 
 
-def vector_tfidf(corpus):
+def crear_vector_tfidf(corpus):
     """Función para vectorizar el corpus por tf-idf"""
 
     vectorizador_tfidf = TfidfVectorizer(token_pattern=r'(?u)\w\w+|\w\w+\n|\.')
     X = vectorizador_tfidf.fit_transform(corpus)
-    print('Representación vectorial tf-idf')
-    print(X.toarray())
+    print('[+]Se creo el vector tf-idf')
+    return X.toarray()
+
+
+def normalizar_corpus(lista_noticias):
+    """Función para normalizar el corpus"""
+
+    nlp = spacy.load("es_core_news_sm")
+
+    # Tokenizar
+    corpus_tokenizado = []
+    for noticia in lista_noticias:
+        # Analisis de texto
+        doc = nlp(noticia)
+        # Tokenización = separación de palabras
+        tokens = [token.text for token in doc]
+        # Lista de tokens a cadena de texto
+        corpus_tokenizado.append(" ".join(tokens))
+
+    # Lematizar
+    corpus_lematizado = []
+    for noticia_tokenizada in corpus_tokenizado:
+        # Analisis de texto
+        doc = nlp(noticia_tokenizada)
+        # Lematización = reducción de palabras a su raíz
+        lemas = [token.lemma_ for token in doc]
+        # Lista de lemas a cadena de texto
+        corpus_lematizado.append(" ".join(lemas))
+
+    # Eliminar palabras vacías
+    # Categorías de palabras para eliminar: articulos, preposiciones, conjunciones y prononmbres
+    categorias_a_eliminar = ["DET", "ADP", "CCONJ", "PRON"]
+    corpus_filtrado = []
+    for noticia_lematizada in corpus_lematizado:
+        # Analizar el texto
+        doc = nlp(noticia_lematizada)
+        # Filtrar palabras según las categorías especificadas
+        tokens_filtrados = [token.text for token in doc if token.pos_ not in categorias_a_eliminar]
+        # Convertir la lista de tokens en una cadena de texto
+        texto_filtrado = " ".join(tokens_filtrados)
+        # Agregar el texto filtrado al nuevo corpus
+        corpus_filtrado.append(texto_filtrado)
+
+    # Retornar el corpus filtrado
+    return corpus_filtrado
